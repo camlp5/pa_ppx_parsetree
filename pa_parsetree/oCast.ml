d7ff9fec69f3a686b8234ababed0f4ff4bf38161
Chet Murthy <chetsky@gmail.com>
2023-02-23 11:31:38 -0800

    meh

--- pa_parsetree/oCast.ml
+++ pa_parsetree/oCast.ml
@@ -1,34 +1,34 @@
 (** $(MIGRATE_OCAMLCFLAGS) -ppopt -pa_import-redeclare -package pa_ppx.import,compiler-libs.common -syntax camlp5o *)
 
 module Lexing = struct
-[%%import: Lexing.position]
+[%%import: Lexing.position] [@@deriving show]
 end
 module Location = struct
-[%%import: Location.t]
-[%%import: 'a Location.loc]
+[%%import: Location.t] [@@deriving show]
+[%%import: 'a Location.loc] [@@deriving show]
 end
 module Longident = struct
-[%%import: Longident.t]
+[%%import: Longident.t] [@@deriving show]
 end
 module Asttypes = struct
-[%%import: Asttypes.constant]
-[%%import: Asttypes.arg_label]
-[%%import: Asttypes.label]
-[%%import: Asttypes.closed_flag]
-[%%import: Asttypes.rec_flag]
-[%%import: Asttypes.direction_flag]
-[%%import: Asttypes.private_flag]
-[%%import: Asttypes.mutable_flag]
-[%%import: Asttypes.virtual_flag]
-[%%import: Asttypes.override_flag]
-[%%import: Asttypes.variance]
-[%%import: Asttypes.injectivity]
+[%%import: Asttypes.constant] [@@deriving show]
+[%%import: Asttypes.arg_label] [@@deriving show]
+[%%import: Asttypes.label] [@@deriving show]
+[%%import: Asttypes.closed_flag] [@@deriving show]
+[%%import: Asttypes.rec_flag] [@@deriving show]
+[%%import: Asttypes.direction_flag] [@@deriving show]
+[%%import: Asttypes.private_flag] [@@deriving show]
+[%%import: Asttypes.mutable_flag] [@@deriving show]
+[%%import: Asttypes.virtual_flag] [@@deriving show]
+[%%import: Asttypes.override_flag] [@@deriving show]
+[%%import: Asttypes.variance] [@@deriving show]
+[%%import: Asttypes.injectivity] [@@deriving show]
 end
-[%%import: Parsetree.constant]
-[%%import: Parsetree.location_stack]
+[%%import: Parsetree.constant] [@@deriving show]
+[%%import: Parsetree.location_stack] [@@deriving show]
 [%%import: Parsetree.attribute
  [@with Asttypes.loc := Location.loc]
-]
+] [@@deriving show]
 [%%import: Parsetree.toplevel_phrase
  [@with Asttypes.loc := Location.loc]
-]
+] [@@deriving show]
