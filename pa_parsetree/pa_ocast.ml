9e7035d5a5bef5b524e95568e2ba4692a73da299
Chet Murthy <chetsky@gmail.com>
2023-02-27 12:11:21 -0800

    baby-stepping

---
+++ pa_parsetree/pa_ocast.ml
@@ -0,0 +1,24 @@
+(** -syntax camlp5r -package camlp5.parser_quotations,camlp5.extend_m *)
+(* camlp5r *)
+(* pa_sexp.ml,v *)
+(* Copyright (c) INRIA 2007-2017 *)
+
+open Asttools;
+open MLast;
+open Pcaml;
+
+value expression_eoi = Grammar.Entry.create gram "expression_eoi";
+
+EXTEND
+  GLOBAL: expr expression_eoi;
+
+  expression: [
+    [
+      e = expr -> MLast2OCast.expr e
+    ]
+  ]
+  ;
+
+  expression_eoi: [ [ x = expression; EOI -> x ] ];
+
+END;
