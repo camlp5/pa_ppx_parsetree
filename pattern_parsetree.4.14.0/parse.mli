58d8418d0bba193cad0446b874850d2cb61b00d1
Chet Murthy <chetsky@gmail.com>
2023-04-11 17:45:14 -0700

    added parse.{ml,mli} to file-compare lists

--- pattern_parsetree.4.14.0/parse.mli
+++ pattern_parsetree.4.14.0/parse.mli
@@ -29,31 +29,31 @@ val expression : Lexing.lexbuf -> Parsetree.expression
 val pattern : Lexing.lexbuf -> Parsetree.pattern
 val module_type : Lexing.lexbuf -> Parsetree.module_type
 val module_expr : Lexing.lexbuf -> Parsetree.module_expr
-val structure_item : Lexing.lexbuf -> Parsetree.structure_item
-val signature_item : Lexing.lexbuf -> Parsetree.signature_item
-val constructor_declaration : Lexing.lexbuf -> Parsetree.constructor_declaration
-val attribute : Lexing.lexbuf -> Parsetree.attribute
-val extension : Lexing.lexbuf -> Parsetree.extension
-val label_declaration : Lexing.lexbuf -> Parsetree.label_declaration
-val match_case : Lexing.lexbuf -> Parsetree.case
-val value_binding : Lexing.lexbuf -> Parsetree.value_binding
-val arg_label : Lexing.lexbuf -> Asttypes.arg_label
-val lident_vala_loc : Lexing.lexbuf -> Ast_helper.str_vala
-val extension_constructor : Lexing.lexbuf -> Parsetree.extension_constructor
-val binding_op : Lexing.lexbuf -> Parsetree.binding_op
-val type_declaration : Lexing.lexbuf -> Parsetree.type_declaration
-val type_substitution : Lexing.lexbuf -> Parsetree.type_declaration
-val constant : Lexing.lexbuf -> Parsetree.constant
-val row_field : Lexing.lexbuf -> Parsetree.row_field
-val object_field : Lexing.lexbuf -> Parsetree.object_field
-val class_description : Lexing.lexbuf -> Parsetree.class_description
-val class_expr : Lexing.lexbuf -> Parsetree.class_expr
-val class_type : Lexing.lexbuf -> Parsetree.class_type
-val class_field : Lexing.lexbuf -> Parsetree.class_field
-val functor_parameter : Lexing.lexbuf -> Parsetree.functor_parameter Ploc.vala
-val module_declaration : Lexing.lexbuf -> Parsetree.module_declaration
-val with_constraint : Lexing.lexbuf -> Parsetree.with_constraint
-val class_type_field : Lexing.lexbuf -> Parsetree.class_type_field
+(*-*)val structure_item : Lexing.lexbuf -> Parsetree.structure_item
+(*-*)val signature_item : Lexing.lexbuf -> Parsetree.signature_item
+(*-*)val constructor_declaration : Lexing.lexbuf -> Parsetree.constructor_declaration
+(*-*)val attribute : Lexing.lexbuf -> Parsetree.attribute
+(*-*)val extension : Lexing.lexbuf -> Parsetree.extension
+(*-*)val label_declaration : Lexing.lexbuf -> Parsetree.label_declaration
+(*-*)val match_case : Lexing.lexbuf -> Parsetree.case
+(*-*)val value_binding : Lexing.lexbuf -> Parsetree.value_binding
+(*-*)val arg_label : Lexing.lexbuf -> Asttypes.arg_label
+(*-*)val lident_vala_loc : Lexing.lexbuf -> Ast_helper.str_vala
+(*-*)val extension_constructor : Lexing.lexbuf -> Parsetree.extension_constructor
+(*-*)val binding_op : Lexing.lexbuf -> Parsetree.binding_op
+(*-*)val type_declaration : Lexing.lexbuf -> Parsetree.type_declaration
+(*-*)val type_substitution : Lexing.lexbuf -> Parsetree.type_declaration
+(*-*)val constant : Lexing.lexbuf -> Parsetree.constant
+(*-*)val row_field : Lexing.lexbuf -> Parsetree.row_field
+(*-*)val object_field : Lexing.lexbuf -> Parsetree.object_field
+(*-*)val class_description : Lexing.lexbuf -> Parsetree.class_description
+(*-*)val class_expr : Lexing.lexbuf -> Parsetree.class_expr
+(*-*)val class_type : Lexing.lexbuf -> Parsetree.class_type
+(*-*)val class_field : Lexing.lexbuf -> Parsetree.class_field
+(*-*)val functor_parameter : Lexing.lexbuf -> Parsetree.functor_parameter Ploc.vala
+(*-*)val module_declaration : Lexing.lexbuf -> Parsetree.module_declaration
+(*-*)val with_constraint : Lexing.lexbuf -> Parsetree.with_constraint
+(*-*)val class_type_field : Lexing.lexbuf -> Parsetree.class_type_field
 
 (** The functions below can be used to parse Longident safely. *)
 
