5f19b5a3116e19c99d50d154f56abcd566f95833
Chet Murthy <chetsky@gmail.com>
2023-04-23 15:44:29 -0700

    add "noattr" quotations and test

--- quotations/q_parsetree.ml
+++ quotations/q_parsetree.ml
@@ -150,6 +150,8 @@ let parse_with_constraint s =
 let parse_class_type_field s =
   lexwrap Pa_ppx_parsetree_pattern_parsetree.Parse.class_type_field s
 
+module Q = struct
+
 [%%import: Reorg_parsetree.attribute]
 [@@deriving q_ast {
     default_data_source_module = Parsetree
@@ -355,5 +357,221 @@ let parse_class_type_field s =
     ; {name = "class_type_field"; from_string = parse_class_type_field ; type_name = class_type_field }
     ]
  }]
+
+end
+
+module QNoAttr = struct
+
+[%%import: Reorg_parsetree.attribute]
+[@@deriving q_ast {
+    default_data_source_module = Parsetree
+  ; default_quotation_source_module = Reorg_parsetree
+  ; expr_meta_module = MetaE
+  ; patt_meta_module = MetaP
+  ; loc_mode = CustomLoc { loc_varname = loc ; loc_type = [%typ: location] ; loc_function_name = location }
+  ; custom_type = [
+      ([%typ: location_stack], {
+         pattern = (fun ctxt _ -> <:patt< _ >>)
+       ; expression = (fun ctxt _ -> <:expr< [] >>)
+       ; function_name = location_stack
+      })
+    ; ([%typ: attributes], {
+         pattern = (fun ctxt _ -> <:patt< _ >>)
+       ; expression = (fun ctxt _ -> <:expr< [] >>)
+       ; function_name = attributes
+      })
+    ]
+  ; pertype = {
+      located = {
+        data_source_module = Location
+      ; quotation_source_module = Reorg_parsetree
+      }
+    ; longident_t = {
+        data_source_module = Longident
+      ; quotation_source_module = Reorg_parsetree
+      }
+    ; arg_label = {
+        data_source_module = Asttypes
+      ; quotation_source_module = Reorg_parsetree
+      }
+    ; arg_label = {
+        data_source_module = Asttypes
+      ; quotation_source_module = Reorg_parsetree
+      }
+    ; rec_flag = {
+        data_source_module = Asttypes
+      ; quotation_source_module = Reorg_parsetree
+      }
+    ; direction_flag = {
+        data_source_module = Asttypes
+      ; quotation_source_module = Reorg_parsetree
+      }
+    ; private_flag = {
+        data_source_module = Asttypes
+      ; quotation_source_module = Reorg_parsetree
+      }
+    ; mutable_flag = {
+        data_source_module = Asttypes
+      ; quotation_source_module = Reorg_parsetree
+      }
+    ; override_flag = {
+        data_source_module = Asttypes
+      ; quotation_source_module = Reorg_parsetree
+      }
+    ; closed_flag = {
+        data_source_module = Asttypes
+      ; quotation_source_module = Reorg_parsetree
+      }
+    ; expression = {
+        add_branches_patt_code = (function
+          | {pexp_desc=Pexp_xtr{txt;loc};pexp_attributes=Ploc.VaVal[];} when antiloc_payload ~loc txt <> "_" -> C.xtr (ploc_of_location loc) txt
+          | {pexp_desc=Pexp_xtr{txt;loc};} when antiloc_payload ~loc txt <> "_" -> failwith "non-blank Pexp_xtr with non-empty attributes is nearly always an error"
+                                 )
+      ; add_branches_expr_code = (function
+          | {pexp_desc=Pexp_xtr{txt;loc};pexp_attributes=Ploc.VaVal[];} -> C.xtr (ploc_of_location loc) txt
+          | {pexp_desc=Pexp_xtr{txt;loc};} -> failwith "Pexp_xtr with non-empty attributes is nearly always an error"
+                                 )
+      }
+    ; expression_desc = {
+        add_branches_patt_code = (function
+          | Pexp_xtr{txt;loc} when antiloc_payload ~loc txt = "_" -> C.xtr (ploc_of_location loc) txt
+                                 )
+      }
+    ; module_expr = {
+        add_branches_patt_code = (function
+          | {pmod_desc=Pmod_xtr{txt;loc};pmod_attributes=Ploc.VaVal[];} when antiloc_payload ~loc txt <> "_" -> C.xtr (ploc_of_location loc) txt
+          | {pmod_desc=Pmod_xtr{txt;loc};} when antiloc_payload ~loc txt <> "_" -> failwith "Pmod_xtr with non-empty attributes is nearly always an error"
+                                 )
+      ; add_branches_expr_code = (function
+          | {pmod_desc=Pmod_xtr{txt;loc};pmod_attributes=Ploc.VaVal[];} -> C.xtr (ploc_of_location loc) txt
+          | {pmod_desc=Pmod_xtr{txt;loc};} -> failwith "Pmod_xtr with non-empty attributes is nearly always an error"
+                                 )
+      }
+    ; module_expr_desc = {
+        add_branches_patt_code = (function
+          | Pmod_xtr{txt;loc} when antiloc_payload ~loc txt = "_" -> C.xtr (ploc_of_location loc) txt
+                                 )
+      }
+    ; module_type = {
+        add_branches_patt_code = (function
+          | {pmty_desc=Pmty_xtr{txt;loc};pmty_attributes=Ploc.VaVal[];} when antiloc_payload ~loc txt <> "_" -> C.xtr (ploc_of_location loc) txt
+          | {pmty_desc=Pmty_xtr{txt;loc};} when antiloc_payload ~loc txt <> "_" -> failwith "Pmty_xtr with non-empty attributes is nearly always an error"
+                                 )
+      ; add_branches_expr_code = (function
+          | {pmty_desc=Pmty_xtr{txt;loc};pmty_attributes=Ploc.VaVal[];} -> C.xtr (ploc_of_location loc) txt
+          | {pmty_desc=Pmty_xtr{txt;loc};} -> failwith "Pmty_xtr with non-empty attributes is nearly always an error"
+                                 )
+      }
+    ; module_type_desc = {
+        add_branches_patt_code = (function
+          | Pmty_xtr{txt;loc} when antiloc_payload ~loc txt = "_" -> C.xtr (ploc_of_location loc) txt
+                                 )
+      }
+
+    ; pattern = {
+        add_branches_patt_code = (function
+          | {ppat_desc=Ppat_xtr{txt;loc};ppat_attributes=Ploc.VaVal[];} when antiloc_payload ~loc txt <> "_" -> C.xtr (ploc_of_location loc) txt
+          | {ppat_desc=Ppat_xtr{txt;loc};} when antiloc_payload ~loc txt <> "_" -> failwith "Ppat_xtr with non-empty attributes is nearly always an error"
+                                 )
+      ; add_branches_expr_code = (function
+          | {ppat_desc=Ppat_xtr{txt;loc};ppat_attributes=Ploc.VaVal[];} -> C.xtr (ploc_of_location loc) txt
+          | {ppat_desc=Ppat_xtr{txt;loc};} -> failwith "Ppat_xtr with non-empty attributes is nearly always an error"
+                                 )
+      }
+    ; pattern_desc = {
+        add_branches_patt_code = (function
+          | Ppat_xtr{txt;loc} when antiloc_payload ~loc txt = "_" -> C.xtr (ploc_of_location loc) txt
+                                 )
+      }
+    ; core_type = {
+        add_branches_patt_code = (function
+          | {ptyp_desc=Ptyp_xtr{txt;loc};ptyp_attributes=Ploc.VaVal[];} when antiloc_payload ~loc txt <> "_" -> C.xtr (ploc_of_location loc) txt
+          | {ptyp_desc=Ptyp_xtr{txt;loc};} when antiloc_payload ~loc txt <> "_" -> failwith "Ptyp_xtr with non-empty attributes is nearly always an error"
+                                 )
+      ; add_branches_expr_code = (function
+          | {ptyp_desc=Ptyp_xtr{txt;loc};ptyp_attributes=Ploc.VaVal[];} -> C.xtr (ploc_of_location loc) txt
+          | {ptyp_desc=Ptyp_xtr{txt;loc};} -> failwith "Ptyp_xtr with non-empty attributes is nearly always an error"
+                                 )
+      }
+    ; core_type_desc = {
+        add_branches_patt_code = (function
+          | Ptyp_xtr{txt;loc} when antiloc_payload ~loc txt = "_" -> C.xtr (ploc_of_location loc) txt
+                                 )
+      }
+    ; class_type = {
+        add_branches_patt_code = (function
+          | {pcty_desc=Pcty_xtr{txt;loc};pcty_attributes=Ploc.VaVal[];} when antiloc_payload ~loc txt <> "_" -> C.xtr (ploc_of_location loc) txt
+          | {pcty_desc=Pcty_xtr{txt;loc};} when antiloc_payload ~loc txt <> "_" -> failwith "Pcty_xtr with non-empty attributes is nearly always an error"
+                                 )
+      ; add_branches_expr_code = (function
+          | {pcty_desc=Pcty_xtr{txt;loc};pcty_attributes=Ploc.VaVal[];} -> C.xtr (ploc_of_location loc) txt
+          | {pcty_desc=Pcty_xtr{txt;loc};} -> failwith "Pcty_xtr with non-empty attributes is nearly always an error"
+                                 )
+      }
+    ; class_type_desc = {
+        add_branches_patt_code = (function
+          | Pcty_xtr{txt;loc} when antiloc_payload ~loc txt = "_" -> C.xtr (ploc_of_location loc) txt
+                                 )
+      }
+    ; class_expr = {
+        add_branches_patt_code = (function
+          | {pcl_desc=Pcl_xtr{txt;loc};pcl_attributes=Ploc.VaVal[];} when antiloc_payload ~loc txt <> "_" -> C.xtr (ploc_of_location loc) txt
+          | {pcl_desc=Pcl_xtr{txt;loc};} when antiloc_payload ~loc txt <> "_" -> failwith "Pcl_xtr with non-empty attributes is nearly always an error"
+                                 )
+      ; add_branches_expr_code = (function
+          | {pcl_desc=Pcl_xtr{txt;loc};pcl_attributes=Ploc.VaVal[];} -> C.xtr (ploc_of_location loc) txt
+          | {pcl_desc=Pcl_xtr{txt;loc};} -> failwith "Pcl_xtr with non-empty attributes is nearly always an error"
+                                 )
+      }
+    ; class_expr_desc = {
+        add_branches_patt_code = (function
+          | Pcl_xtr{txt;loc} when antiloc_payload ~loc txt = "_" -> C.xtr (ploc_of_location loc) txt
+                                 )
+      }
+
+    ; str_vala = {
+        data_source_module = Ast_helper
+      ; quotation_source_module = Reorg_parsetree
+      }
+    }
+  ; entrypoints = [
+      {name = "expression.noattr"; from_string = parse_expression ; type_name = expression }
+    ; {name = "module_expr.noattr"; from_string = parse_module_expr ; type_name = module_expr }
+    ; {name = "module_type.noattr"; from_string = parse_module_type ; type_name = module_type }
+    ; {name = "pattern.noattr"; from_string = parse_pattern ; type_name = pattern }
+    ; {name = "constant.noattr"; from_string = parse_constant ; type_name = constant }
+    ; {name = "core_type.noattr"; from_string = parse_core_type ; type_name = core_type }
+    ; {name = "longident_t.noattr"; from_string = parse_longident ; type_name = longident_t }
+    ; {name = "longlident.noattr"; from_string = parse_longlident ; type_name = longident_t }
+    ; {name = "extended_module_path.noattr"; from_string = parse_extended_module_path ; type_name = longident_t }
+    ; {name = "structure_item.noattr"; from_string = parse_structure_item ; type_name = structure_item }
+    ; {name = "structure.noattr"; from_string = parse_structure ; type_name = structure }
+    ; {name = "signature_item.noattr"; from_string = parse_signature_item ; type_name = signature_item }
+    ; {name = "signature.noattr"; from_string = parse_signature ; type_name = signature }
+    ; {name = "constructor_declaration.noattr"; from_string = parse_constructor_declaration ; type_name = constructor_declaration }
+    ; {name = "attribute.noattr"; from_string = parse_attribute ; type_name = attribute }
+    ; {name = "extension.noattr"; from_string = parse_extension ; type_name = extension }
+    ; {name = "label_declaration.noattr"; from_string = parse_label_declaration ; type_name = label_declaration }
+    ; {name = "case.noattr"; from_string = parse_match_case ; type_name = case }
+    ; {name = "value_binding.noattr"; from_string = parse_value_binding ; type_name = value_binding }
+    ; {name = "arg_label.noattr"; from_string = parse_arg_label ; type_name = arg_label }
+    ; {name = "lident_loc.noattr"; from_string = parse_lident_vala_loc ; type_name = str_vala }
+    ; {name = "extension_constructor.noattr"; from_string = parse_extension_constructor ; type_name = extension_constructor }
+    ; {name = "binding_op.noattr"; from_string = parse_binding_op ; type_name = binding_op }
+    ; {name = "type_decl.noattr"; from_string = parse_type_declaration ; type_name = type_declaration }
+    ; {name = "type_subst.noattr"; from_string = parse_type_substitution ; type_name = type_declaration }
+    ; {name = "row_field.noattr"; from_string = parse_row_field ; type_name = row_field }
+    ; {name = "object_field.noattr"; from_string = parse_object_field ; type_name = object_field }
+    ; {name = "class_description.noattr"; from_string = parse_class_description ; type_name = class_description }
+    ; {name = "class_expr.noattr"; from_string = parse_class_expr ; type_name = class_expr }
+    ; {name = "class_type.noattr"; from_string = parse_class_type ; type_name = class_type }
+    ; {name = "class_field.noattr"; from_string = parse_class_field ; type_name = class_field }
+    ; {name = "functor_parameter.noattr"; from_string = parse_functor_parameter ; type_name = functor_parameter_vala }
+    ; {name = "module_declaration.noattr"; from_string = parse_module_declaration ; type_name = module_declaration }
+    ; {name = "with_constraint.noattr"; from_string = parse_with_constraint ; type_name = with_constraint }
+    ; {name = "class_type_field.noattr"; from_string = parse_class_type_field ; type_name = class_type_field }
+    ]
+ }]
+end
 
 end
